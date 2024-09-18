bd6a5ebae53e04b8f0bb3800f4a7e0be115d1e5d
Beulah124 <sekhar.cse@cmrtc.ac.in>
2024-09-18 13:13:29 +0530

    new

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Jenkins is tool or server, which continouosly integrates, buils, and test");
+
     }
 }
